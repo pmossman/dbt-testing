a2bd3b7b99454843a805ab8d9d779da8920d5058
pmossman <parker@airbyte.io>
2022-05-09 13:24:11 -0700

    add dbt project

---
+++ models/ash.sql
@@ -0,0 +1,24 @@
+with pokemon as (
+  select
+    id as pokemon_id,
+    name as pokemon_name
+  from pokemon
+),
+
+move as (
+  select name as move_name
+  from pokemon_moves_move
+),
+
+ashes_team as (
+  select
+    pokemon.pokemon_name,
+    pokemon.pokemon_id,
+    move.move_name
+
+  from pokemon
+
+  inner join move on (pokemon.pokemon_name = 'pikachu' and move.move_name = 'surf')
+)
+
+select distinct * from ashes_team
